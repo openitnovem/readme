d0885f81e46557569b3623cec3a8e6da302295a5
FSII03941 <soumaya.ihihi@sncf.fr>
2020-09-02 20:15:01 +0200

    WIP: Add tests

--- tests/test_visualization/test_visualization.py
+++ tests/test_visualization/test_visualization.py
@@ -1,16 +1,34 @@
-from fbd_interpreter.visualization.plots import interpretation_plots_to_html_report
+import os
+
 import plotly.graph_objects as go
-from fbd_interpreter.logger import logger
+from bs4 import BeautifulSoup
+
+from fbd_interpreter.config.load import configuration
+from fbd_interpreter.logger import ROOT_DIR, logger
+from fbd_interpreter.visualization.plots import interpretation_plots_to_html_report
+
+# Get html sections path
+html_sections = configuration["DEV"]["html_sections"]
 
 
-def test_plots() -> None:
+def test_interpretation_plots_to_html_report() -> None:
     # Create dummy figure to plot as html with available content of sections
-    f = go.FigureWidget()
-    f.add_scatter(y=[2, 1, 4, 3]);
-    dict_figures = {"DUMMY":f}
+    pdp_dummy_figure = go.FigureWidget()
+    pdp_dummy_figure.add_scatter(y=[2, 1, 4, 3])
+    dict_figures = {"PDP": pdp_dummy_figure}
     logger.info("f")
-    html = interpretation_plots_to_html_report(dict_figures)
-    print(html)
-    assert 1==1
-
-#test_plots()
+    out_path = os.path.join(ROOT_DIR, "../outputs/tests/dummy_figure.html")
+    header_pdp = "Dummy figure"
+    html = interpretation_plots_to_html_report(
+        dict_figures,
+        html_sections=html_sections,
+        plot_type="PDP",
+        title=header_pdp,
+        path=os.path.join(ROOT_DIR, "../outputs/tests/dummy_pdp_figure.html"),
+    )
+    # Check if html is valid
+    assert bool(BeautifulSoup(html, "html.parser").find())
+    # Check if html contains title
+    assert header_pdp in html
+    # Check if file is created
+    assert os.path.exists(out_path)
